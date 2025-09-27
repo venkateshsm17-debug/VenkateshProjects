4e32aab7bf50e1661e89785b0b6b2f52af871b97
user <user@DESKTOP-036HN35>
2025-09-27 13:49:34 +0530

    First Commit

---
+++ DDT.Cucumber/src/test/java/StepDef/LoginPage.java
@@ -0,0 +1,45 @@
+package StepDef;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPage {
+	WebDriver driver=null;
+	
+	@Given("browser is open")
+	public void browser_is_open() {
+	    driver=new ChromeDriver();
+	    
+	}
+
+	@Given("user is on login page")
+	public void user_is_on_login_page() throws InterruptedException {
+	    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	    Thread.sleep(5000);
+	}
+
+	@When("^user enters (.*) and (.*)$")
+	public void user_enters_username_and_password(String username,String password) {
+	    driver.findElement(By.name("username")).sendKeys(username);
+	    driver.findElement(By.name("password")).sendKeys(password);
+	}
+
+	@When("user clicks on login")
+	public void user_clicks_on_login() {
+	    driver.findElement(By.xpath("//button[@Type='submit']")).click();
+	}
+
+	@Then("user is navigated to the home page")
+	public void user_is_navigated_to_the_home_page() {
+	    driver.findElement(By.linkText("Dashboard")).isDisplayed();
+	}
+
+	
+
+
+}
